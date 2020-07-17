dd6e85ccf6e59000e33cd1a7cf18d3900633803b
thevickypedia <vignesh.sivanandarao@gmail.com>
2020-07-17 18:09:38 -0500

    include pagination describe_log_streams will only retrieve 50 log streams at once

--- invisible.py
+++ invisible.py
@@ -4,20 +4,26 @@ from datetime import datetime, timedelta
 import boto3
 
 client = boto3.client('logs')
-app_name = 'stock_hawk'
+app_name = 'robinhood'
 
-response = client.describe_log_streams(
+paginator = client.get_paginator('describe_log_streams')
+response_iterator = paginator.paginate(
     logGroupName=f'/aws/lambda/{app_name}',
 )
+for a in response_iterator:
+    b = a['logStreams']
+    for c in b:
+        print(c['logStreamName'])
 
-tod = datetime.today() - timedelta(days=65)
-epoch_date = str(math.trunc(tod.timestamp()))
-deletion_date = int(epoch_date.ljust(13, '0'))
 
-for item in (response['logStreams']):
-    if item['creationTime'] < deletion_date:
-        resp = client.delete_log_stream(
-            logGroupName=f'/aws/lambda/{app_name}',
-            logStreamName=f"{item['logStreamName']}"
-        )
-        print(resp)
+# tod = datetime.today() - timedelta(days=65)
+# epoch_date = str(math.trunc(tod.timestamp()))
+# deletion_date = int(epoch_date.ljust(13, '0'))
+
+# for item in (response['logStreams']):
+#     if item['creationTime'] < deletion_date:
+#         resp = client.delete_log_stream(
+#             logGroupName=f'/aws/lambda/{app_name}',
+#             logStreamName=f"{item['logStreamName']}"
+#         )
+#         print(resp)
