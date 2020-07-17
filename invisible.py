ae77e26c2b727be0ea47041f45a732a064f33e09
thevickypedia <vignesh.sivanandarao@gmail.com>
2020-07-17 18:43:24 -0500

    Optimization: maintain coding standards

--- invisible.py
+++ invisible.py
@@ -5,31 +5,41 @@ import boto3
 
 app_name = 'robinhood'
 
-client = boto3.client('logs')
-paginator = client.get_paginator('describe_log_streams')
-response_iterator = paginator.paginate(
-    logGroupName=f'/aws/lambda/{app_name}',
-)
+
+def login():
+    client = boto3.client('logs')
+    paginator = client.get_paginator('describe_log_streams')
+    response_iterator = paginator.paginate(
+        logGroupName=f'/aws/lambda/{app_name}',
+    )
+    return client, response_iterator
 
 
 def deletion_date():
-    tod = datetime.today() - timedelta(days=20)
+    tod = datetime.today() - timedelta(days=7)
     epoch_date = str(math.trunc(tod.timestamp()))
     selected_date = int(epoch_date.ljust(13, '0'))
     return selected_date
 
 
-print('Deleting log files..')
+def purger():
+    print('Deleting log files..')
+    for item in response:
+        collection = item['logStreams']
+        for collected_value in collection:
+            if collected_value['creationTime'] < req_date:
+                resp = client_.delete_log_stream(
+                    logGroupName=f'/aws/lambda/{app_name}',
+                    logStreamName=f"{collected_value['logStreamName']}"
+                )
+                if (resp['ResponseMetadata']['HTTPStatusCode']) == 200:
+                    pass
+                else:
+                    print(f"Unable to purge logStream: {collected_value['logStreamName']}")
+                    pass
 
-for response in response_iterator:
-    collection = response['logStreams']
-    for collected_value in collection:
-        if collected_value['creationTime'] < deletion_date():
-            resp = client.delete_log_stream(
-                logGroupName=f'/aws/lambda/{app_name}',
-                logStreamName=f"{collected_value['logStreamName']}"
-            )
-            if (resp['ResponseMetadata']['HTTPStatusCode']) == 200:
-                pass
-            else:
-                print(f"Unable to purge logStream: {collected_value['logStreamName']}")
+
+if __name__ == '__main__':
+    client_, response = login()
+    req_date = deletion_date()
+    print(f'\n{purger()} log streams were purged successfully.')
