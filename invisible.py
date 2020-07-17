ece202b2ef2e3fa8faea4f022cea833ba675ba57
thevickypedia <vignesh.sivanandarao@gmail.com>
2020-07-17 18:07:42 -0500

    reformat date lookup

--- invisible.py
+++ invisible.py
@@ -10,11 +10,9 @@ response = client.describe_log_streams(
     logGroupName=f'/aws/lambda/{app_name}',
 )
 
-tod = datetime.today() - timedelta(days=2)
-epoch_date = math.trunc(tod.timestamp())
-epoch_date = str(epoch_date)
-deletion_date = epoch_date.ljust(13, '0')
-deletion_date = int(deletion_date)
+tod = datetime.today() - timedelta(days=65)
+epoch_date = str(math.trunc(tod.timestamp()))
+deletion_date = int(epoch_date.ljust(13, '0'))
 
 for item in (response['logStreams']):
     if item['creationTime'] < deletion_date:
