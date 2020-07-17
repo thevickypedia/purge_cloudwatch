13d34edab9baa48f1aef086b3ebcba92208349fa
thevickypedia <vignesh.sivanandarao@gmail.com>
2020-07-17 18:24:10 -0500

    check date on paginated log files

--- invisible.py
+++ invisible.py
@@ -3,27 +3,33 @@ from datetime import datetime, timedelta
 
 import boto3
 
-client = boto3.client('logs')
 app_name = 'robinhood'
 
+client = boto3.client('logs')
 paginator = client.get_paginator('describe_log_streams')
 response_iterator = paginator.paginate(
     logGroupName=f'/aws/lambda/{app_name}',
 )
-for a in response_iterator:
-    b = a['logStreams']
-    for c in b:
-        print(c['logStreamName'])
 
 
-# tod = datetime.today() - timedelta(days=65)
-# epoch_date = str(math.trunc(tod.timestamp()))
-# deletion_date = int(epoch_date.ljust(13, '0'))
+def deletion_date():
+    tod = datetime.today() - timedelta(days=20)
+    epoch_date = str(math.trunc(tod.timestamp()))
+    selected_date = int(epoch_date.ljust(13, '0'))
+    return selected_date
 
-# for item in (response['logStreams']):
-#     if item['creationTime'] < deletion_date:
-#         resp = client.delete_log_stream(
-#             logGroupName=f'/aws/lambda/{app_name}',
-#             logStreamName=f"{item['logStreamName']}"
-#         )
-#         print(resp)
+
+print('Deleting log files..')
+
+for response in response_iterator:
+    collection = response['logStreams']
+    for collected_value in collection:
+        if collected_value['creationTime'] < deletion_date():
+            resp = client.delete_log_stream(
+                logGroupName=f'/aws/lambda/{app_name}',
+                logStreamName=f"{collected_value['logStreamName']}"
+            )
+            if (resp['ResponseMetadata']['HTTPStatusCode']) == 200:
+                pass
+            else:
+                print(f"Unable to purge logStream: {collected_value['logStreamName']}")
